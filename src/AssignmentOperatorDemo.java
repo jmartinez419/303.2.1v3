f578ff859c9db62c8269b784443eec43035d10db
jmartinez419 <justynmartinez419@gmail.com>
2024-06-26 12:33:43 -0400

    did lab via instructions and wrote out the code that they presented

---
+++ src/AssignmentOperatorDemo.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class AssignmentOperatorDemo {
+    public static void main(String[] args) {
+                //   Assigning Primitive Values
+                int j, k;
+                j = 10; // j gets the value 10.
+                j = 5; // j gets the value 5. The previous value is overwritten.
+                k = j; // k gets the value 5.
+                System.out.println("j is : " + j);
+                System.out.println("k is : " + k);
+
+                //              Multiple Assignments
+                k = j = 10; // (k = (j = 10))
+                System.out.println("j is : " + j);
+                System.out.println("k is : " + k);
+            }
+
+    }
